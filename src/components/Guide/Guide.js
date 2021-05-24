72d7b2fc57ad1da3a409babbfc71d45160acf7a2
JiAn <longja05@email.com>
2021-05-24 15:05:49 +0800

    added skeleton for the guide

--- src/components/Guide/Guide.js
+++ src/components/Guide/Guide.js
@@ -5,32 +5,65 @@ const Guide = () => {
   return (
     <div className="guide">
       <div className="container">
-        <div className="row">
           <div className="col-md-12">
             <div className="section-heading">
-              <h2>How it works!</h2>
+              <h2>How it works</h2>
             </div>
           </div>
+          <div className = "sub-section-heading">
+            <h3>For the seller:</h3>
+            </div>
           <div className = "infographics">
-          <div className="col-md-6">
+          <div className="col-4">
             <div className="left-content">
             <img src="assets/images/feature-image.jpg" alt="picture here" />
+            <h4>List your product or service</h4>
+            <p>Select the cateogory in which your product or service belongs to and simply click on New Listing</p>
             </div>
           </div>
-          <div className="col-md-6">
+          <div className="col-4">
             <div className="middle-content">
               <img src="assets/images/feature-image.jpg" alt="picture here" />
+              <h4>Chat with potential buyers</h4>
+            <p>Once one or more potential buyers makes an offer, a chat function will be availale for you to chat with your buyer!</p>
             </div>
           </div>
-          <div className="col-md-6">
+          <div className="col-4">
             <div className="right-content">
               <img src="assets/images/feature-image.jpg" alt="picture here" />
+              <h4>Sell</h4>
+            <p>Finalise the deal once the buyer or buyers have confirmed their modes of payment and delivery</p>
+            </div>
+          </div>
+          </div>
+          <div className = "sub-section-heading">
+            <h3>For the buyer:</h3>
+            </div>
+          <div className = "infographics">
+          <div className="col-4">
+            <div className="left-content">
+            <img src="assets/images/feature-image.jpg" alt="picture here" />
+            <h4>Browse the categories</h4>
+            <p>Select the cateogory which you are interested in the browse the products or services in that category</p>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="middle-content">
+              <img src="assets/images/feature-image.jpg" alt="picture here" />
+              <h4>Chat with the sellers</h4>
+            <p>Found a product or service you are interested in? Go ahead an list an offer or chat with the seller to know more</p>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="right-content">
+              <img src="assets/images/feature-image.jpg" alt="picture here" />
+              <h4>Buy</h4>
+            <p>Once you have agreed on the modes of payment and delivery with the seller, just finalise the deal</p>
             </div>
           </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
