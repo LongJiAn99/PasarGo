d54bcc45f52710be88ae60fc0cb94c63abaf8618
JiAn <longja05@email.com>
2021-05-26 15:34:28 +0800

    added firebase and changed the navbar

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import './bootstrap/css/bootstrap.min.css';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from 'react-router-dom'
+import { FirebaseAuthProvider } from '@react-firebase/auth';
+import { config } from './config/firebase';
+import { firebase } from "@firebase/app";
+import "@firebase/auth";
 
 ReactDOM.render(
   <Router>
     <React.StrictMode>
+      <FirebaseAuthProvider {...config} firebase = {firebase}>
       <App />
+      </FirebaseAuthProvider>
     </React.StrictMode>
   </Router>,
   document.getElementById('root')
--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,49 +1,112 @@
 import React, { Component } from "react";
+import { useState } from "react";
 import { NavHashLink } from "react-router-hash-link";
 import { FaBars } from "react-icons/fa";
-import './css/Header.css'
+import "./css/Header.css";
 import logo from "./images/logo.jpg";
+import { IfFirebaseAuthed, IfFirebaseUnAuthed } from "@react-firebase/auth";
+import { CgProfile } from "react-icons/cg";
+import {
+  Menu,
+  MenuItem,
+} from "@material-ui/core";
 
 export default class Navbar extends Component {
   state = {
-    isOpen: false
+    isOpen: false,
   };
   handleToggle = () => {
     this.setState({ isOpen: !this.state.isOpen });
   };
+
   render() {
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+  
+    const handleLogout = (firebase) => {
+      handleClose();
+      firebase.auth().signOut();
+    };
+
     return (
       <nav className="navbar">
         <div className="nav-center">
           <div className="nav-header">
             <NavHashLink smooth to="#home">
-              <div className = 'logobrand'>
-              <img src={logo} width="100px" alt="" />
-              <h2>Pasar<em>Go!</em></h2>
+              <div className="logobrand">
+                <img src={logo} width="100px" alt="" />
+                <h2>
+                  Pasar<em>Go!</em>
+                </h2>
               </div>
             </NavHashLink>
             <button
               type="button"
               className="nav-btn"
               onClick={this.handleToggle}
-            ><FaBars className="nav-icon" />
+            >
+              <FaBars className="nav-icon" />
             </button>
           </div>
-          <ul className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}>
+          <ul
+            className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
+          >
             <li>
-              <NavHashLink smooth to="#home">Home</NavHashLink>
+              <NavHashLink smooth to="#home">
+                Home
+              </NavHashLink>
             </li>
             <li>
-              <NavHashLink smooth to="#categories">Browse</NavHashLink>
+              <NavHashLink smooth to="#categories">
+                Browse
+              </NavHashLink>
             </li>
             <li>
-              <NavHashLink smooth to="#guide">Guide</NavHashLink>
+              <NavHashLink smooth to="#guide">
+                Guide
+              </NavHashLink>
             </li>
             <li>
-              <NavHashLink smooth to="#about">About</NavHashLink>
+              <NavHashLink smooth to="#about">
+                About
+              </NavHashLink>
             </li>
             <li>
-              <NavHashLink smooth to="#signup">Login</NavHashLink>
+              <IfFirebaseUnAuthed>
+                {({ user, firebase }) => (
+                  <div>
+                    <CgProfile
+                      aria-controls="simple-menu"
+                      aria-haspopup="true"
+                      onClick={handleClick}
+                    />
+
+                    <Menu
+                      id="simple-menu"
+                      anchorEl={anchorEl}
+                      keepMounted
+                      open={Boolean(anchorEl)}
+                      onClose={handleClose}
+                    >
+                      <MenuItem onClick={() => handleLogout(firebase)}>
+                        Logout
+                      </MenuItem>
+                    </Menu>
+                  </div>
+                )}
+              </IfFirebaseUnAuthed>
+              <IfFirebaseAuthed>
+                <NavHashLink smooth to="#signup">
+                  Log out
+                </NavHashLink>
+              </IfFirebaseAuthed>
             </li>
           </ul>
         </div>
