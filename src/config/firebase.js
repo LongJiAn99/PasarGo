ce748473fb2cb9844bbfcd5888c31e764e11d037
xin yen <xinyen1910@gmail.com>
2021-05-26 22:37:06 +0800

    firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export const config ={
+    apiKey: "AIzaSyDzMTte3agfDwS5POaSOfMFNjlabgfdxcw",
+    authDomain: "pasargo-faf3a.firebaseapp.com",
+    projectId: "pasargo-faf3a",
+    storageBucket: "pasargo-faf3a.appspot.com",
+    messagingSenderId: "920278672871",
+    appId: "1:920278672871:web:06ce3ad49b28519ebb0942",
+};
