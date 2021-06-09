5be3d328f4ecde96f88cbd6441ee38c987439292
JiAn <longja05@email.com>
2021-06-09 17:40:54 +0800

    added all the categories routing

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import firebase from "firebase/app";
 import "firebase/firestore";
-import { Grid } from '@material-ui/core'
+import { Grid, Container } from '@material-ui/core'
 import Product from './Product'
 import { useLocation } from "react-router-dom";
+import './css/Products.css'
 
 
 function Products(props) {
@@ -34,7 +35,7 @@ function Products(props) {
 
   return (
     <div>
-      <h1>{category}</h1>
+      <h2>{category}</h2>
       <Grid container justify='center' spacing = {4}>
         {products.map((product) => (
             <Grid item key = {product.id} xs = {12} sm = {6} md ={4} lg = {3}>
--- src/components/Scrollbar.js
+++ src/components/Scrollbar.js
@@ -1,101 +1,152 @@
 import React from "react";
 import "./css/Scrollbar.css";
 import foodicon from "../images/foodicon.png";
-import womenfashionicon from '../images/womenfashionicon.png'
-import menfashionicon from '../images/menfashionicon.png'
-import artsicon from '../images/artsicon.png'
-import beautyicon from '../images/beautyicon.png'
-import eventicon from '../images/eventicon.png'
-import peticon from '../images/peticon.png'
-import eduicon from '../images/eduicon.png'
-import webicon from '../images/webicon.png'
+import womenfashionicon from "../images/womenfashionicon.png";
+import menfashionicon from "../images/menfashionicon.png";
+import artsicon from "../images/artsicon.png";
+import beautyicon from "../images/beautyicon.png";
+import eventicon from "../images/eventicon.png";
+import peticon from "../images/peticon.png";
+import eduicon from "../images/eduicon.png";
+import webicon from "../images/webicon.png";
 import { Link } from "react-router-dom";
 
 const Scrollbar = () => {
   return (
     <div className="scrollbar">
       <div className="col-md-2">
-        <Link to={{
-          pathname: '../pages/listings',
-          state: {
-            category: 'food'
-          }
-        }}>
         <div className="product-item">
+          <Link
+            to={{
+              pathname: "../pages/listings",
+              state: {
+                category: "food",
+              },
+            }}
+          >
             <img src={foodicon} />
             <h3>Food</h3>
+          </Link>
         </div>
-        </Link>
       </div>
       <div className="col-md-2">
         <div className="product-item">
-          <a href="#">
+          <Link
+            to={{
+              pathname: "../pages/listings",
+              state: {
+                category: "men's fashion",
+              },
+            }}
+          >
             <img src={menfashionicon} />
             <h3>Men's Fashion</h3>
-          </a>
+          </Link>
         </div>
       </div>
       <div className="col-md-2">
         <div className="product-item">
-          <a href="#">
+          <Link
+            to={{
+              pathname: "../pages/listings",
+              state: {
+                category: "women's fashion",
+              },
+            }}
+          >
             <img src={womenfashionicon} />
             <h3>Women's Fashion</h3>
-          </a>
+          </Link>
         </div>
       </div>
       <div className="col-md-2">
         <div className="product-item">
-          <a href="#">
+          <Link
+            to={{
+              pathname: "../pages/listings",
+              state: {
+                category: "arts and craft",
+              },
+            }}
+          >
             <img src={artsicon} />
             <h3>Arts & Craft</h3>
-          </a>
+          </Link>
         </div>
       </div>
       <div className="col-md-2">
-      <Link to={{
-          pathname: '../pages/listings',
-          state: {
-            category: 'health and beauty'
-          }
-        }}>
         <div className="product-item">
-          <a href="#">
+          <Link
+            to={{
+              pathname: "../pages/listings",
+              state: {
+                category: "health and beauty",
+              },
+            }}
+          >
             <img src={beautyicon} />
             <h3>Health & Beauty</h3>
-          </a>
+          </Link>
         </div>
-        </ Link>
       </div>
       <div className="col-md-2">
         <div className="product-item">
-          <a href="#">
+        <Link
+            to={{
+              pathname: "../pages/listings",
+              state: {
+                category: "event planning",
+              },
+            }}
+          >
             <img src={eventicon} />
             <h3>Event Planning</h3>
-          </a>
+          </ Link>
         </div>
       </div>
       <div className="col-md-2">
         <div className="product-item">
-          <a href="#">
+        <Link
+            to={{
+              pathname: "../pages/listings",
+              state: {
+                category: "pet grooming",
+              },
+            }}
+          >
             <img src={peticon} />
             <h3>Pet Grooming</h3>
-          </a>
+          </Link>
         </div>
       </div>
       <div className="col-md-2">
         <div className="product-item">
-          <a href="#">
+        <Link
+            to={{
+              pathname: "../pages/listings",
+              state: {
+                category: "education",
+              },
+            }}
+          >
             <img src={eduicon} />
             <h3>Education</h3>
-          </a>
+          </Link>
         </div>
       </div>
       <div className="col-md-2">
         <div className="product-item">
-          <a href="#">
+        <Link
+            to={{
+              pathname: "../pages/listings",
+              state: {
+                category: "web design",
+              },
+            }}
+          >
             <img src={webicon} />
             <h3>Web Design</h3>
-          </a>
+          </ Link>
         </div>
       </div>
     </div>
