38c97f16ea13e78dde8192e846d66f91babcc31a
xin yen <xinyen1910@gmail.com>
2021-06-26 16:17:38 +0800

    wishlist and listing page

--- src/components/Listings/Sections/Datas.js
+++ src/components/Listings/Sections/Datas.js
@@ -29,43 +29,6 @@ const price = [
 
 
 
-const categories = [
-    {
-        "_id": 1,
-        "name": "Arts and Crafts "
-    },
-    {
-        "_id": 2,
-        "name": "Education"
-    },
-    {
-        "_id": 3,
-        "name": "Event Planning"
-    },
-    {
-        "_id": 4,
-        "name": "food"
-    },
-    {
-        "_id": 5,
-        "name": "health and beauty"
-    },
-    {
-        "_id": 6,
-        "name": "men's fashion"
-    },
-    {
-        "_id": 7,
-        "name": "women's fashion"
-    },
-    {
-        "_id": 8,
-        "name": "pet grooming"
-    },
-]
-
-
 export {
-    price,
-    categories
+    price
 }
